570485d4c12a6c3eb3c1432c722de5e8a6197119
agiratech-devanathan <devanathan@agiratech.com>
2022-05-04 15:33:06 +0530

    feat : Add conditional rendering of the react elemetn using if and ternary operators.

--- my-app/src/components/Post/post.js
+++ my-app/src/components/Post/post.js
@@ -10,63 +10,58 @@ class Post extends Component {
             { title: 'post title 1', description: 'Post description1' },
             { title: 'post title 2', description: 'Post description2' }
         ],
-        postTitle: 'Post List'
+        postTitle: 'Post List',
+        showPost: false,
+        count: true
     }
 
-    updateTitleHandler(title,e) {
-        e.preventDefault();
-        console.log('update title with function')
-        console.log(this)
+    togglePostHandler = () => {
         this.setState({
-            postTitle: title
+            showPost: !this.state.showPost
         })
-
     }
-    titleHandler = (title,e) => {
-        e.preventDefault();
-        console.log('update title with property function')
-        console.log(this)
-        this.setState({
-            postTitle: title
-        })
 
+    getPost() {
+        if (!this.state.showPost) return null;
+        return (<div className='post-content'>
+            <SinglePost title={this.state.posts[0].title} description={this.state.posts[0].description} />
+            <SinglePost title={this.state.posts[1].title} description={this.state.posts[1].description} />
+        </div>)
     }
     render() {
-        setTimeout(() => {
-            const ModifiedPosts = [...this.state.posts];
-            ModifiedPosts[0].title = 'Modifief posts Data 1';
-            ModifiedPosts[1].title = 'Modifief posts Data 2';
-
-            this.setState({
-                ModifiedPosts,
-            })
-            this.setState({
-                postTitle: 'Modified Post title',
-            })
-        }, 3000)
+        let post = null;
+        if (this.state.showPost) {
+            // post = (<div className='post-content'>
+            //     <SinglePost title={this.state.posts[0].title} description={this.state.posts[0].description} />
+            //     <SinglePost title={this.state.posts[1].title} description={this.state.posts[1].description} />
+            // </div>)
+        }
         return (
 
             <div className='post-container'>
+                <div>{this.state.count && 'show count'}</div>
                 <div className='post-header'>
                     <h2>{this.state.postTitle} </h2>
                 </div>
                 <div className='update-btn-container'>
-              
-                    <a href='https://www.google.com/' 
-                    onClick={this.titleHandler.bind(this,'code world')} className='update-btn'>update Post with function</a>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <a href='https://www.google.com/' 
-                    onClick={this.updateTitleHandler.bind(this,'modified title')} className='update-btn'>update Post with property function</a>
-                     <button onClick={this.updateTitleHandler.bind(this)} className='update-btn'>update Post title</button> 
+                    <button
+                        onClick={this.togglePostHandler}
+                        className='toggle-btn'>{this.state.showPost ? 'Hide post' : 'Show post'}</button>
                 </div>
                 <hr />
-                <div className='post-content'>
+                {/* {post} */}
+                {/* this is one of the method to show element using ternary operators */}
+                {/* {this.state.showPost ? (<div className='post-content'>
                     <SinglePost title={this.state.posts[0].title} description={this.state.posts[0].description} />
                     <SinglePost title={this.state.posts[1].title} description={this.state.posts[1].description} />
-                </div>
-
+                </div>) : null} */}
+                {/* this is another of the method to show element using ternary operators without else part using && symbol */}
+                {/* {this.state.showPost && (<div className='post-content'>
+                    <SinglePost title={this.state.posts[0].title} description={this.state.posts[0].description} />
+                    <SinglePost title={this.state.posts[1].title} description={this.state.posts[1].description} />
+                </div>)} */}
+                {/* this is another of the method to show element using function if else operators*/}
+                {this.getPost()}
             </div>
         );
     }
